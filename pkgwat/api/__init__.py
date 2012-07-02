5b621d2d39e640cc104900622a0bc19f5cbbea75
Ralph Bean <rbean@redhat.com>
2012-07-02 14:38:05 -0400

    Split out from pkgwat.cli

---
+++ pkgwat/api/__init__.py
@@ -0,0 +1,6 @@
+__name__ = 'pkgwat.api'
+__version__ = "0.1"
+__description__ = "Python API for querying the fedora packages webapp"
+__author__ = "Ralph Bean"
+__author_email__ = "rbean@redhat.com"
+__url__ = "http://github.com/ralphbean/pkgwat.api"
