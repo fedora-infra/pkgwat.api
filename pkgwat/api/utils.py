fedora-infra/pkgwat.api

commit 8fe5aed3e64017c625a3084262360b8d05eb8658

Make the "1 karma" show up more distinct and not smashed against the preceding text.

pkgwat/api/utils.py
@@ -13,7 +13,7 @@ class MLStripper(html_parser.HTMLParser):
         self.fed.append(d)
 
     def get_data(self):
-        return ''.join(self.fed)
+        return ', '.join(self.fed)
 
 
 def strip_tags(d):

commit 6be374eaa4ba238f9902fa2a67c1c17b9bc82b5b

Fix a bug introduced in 8fe5aed when searching for.. anything.

pkgwat/api/utils.py
@@ -13,7 +13,7 @@ class MLStripper(html_parser.HTMLParser):
         self.fed.append(d)
 
     def get_data(self):
-        return ', '.join(self.fed)
+        return ' '.join([item.strip() for item in self.fed])
 
 
 def strip_tags(d):