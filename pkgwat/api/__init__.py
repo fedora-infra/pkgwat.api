58db943211afbf3b00a1eb71d7971e9cbc8a0c3d
Ralph Bean <rbean@redhat.com>
2013-02-25 21:21:35 -0500

    PEP8.

--- pkgwat/api/__init__.py
+++ pkgwat/api/__init__.py
@@ -50,7 +50,7 @@ try:
     # This only works on older versions of python-requests
     import requests.defaults
     requests.defaults.defaults['base_headers']['User-Agent'] = \
-            'pkgwat/' + __version__
+        'pkgwat/' + __version__
 except ImportError:
     # If it doesn't exist, no biggie.  But we should fix this in the future.
     pass
@@ -223,7 +223,7 @@ def releases(package, rows_per_page=10, start_row=0, strip_tags=True):
 
 
 def builds(package, state='all', rows_per_page=10,
-             start_row=0, strip_tags=True):
+           start_row=0, strip_tags=True):
     """ Retrieve a list of the latest koji builds for a package.
 
     :view: https://apps.fedoraproject.org/packages/ccze/builds
